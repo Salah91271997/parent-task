9f67340d95bc26197baf38362377ee48b48a9c47
Salah Ebrahim <47260975+Salah91271997@users.noreply.github.com>
2023-08-10 14:57:02 +0300

    expanding bundle size , comment on lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
+import { AuthGuard } from './guards/auth/auth.guard';
+import { LoginGuard } from './guards/auth/login.guard';
 import { LoginComponent } from './components/login/login.component';
 import { UsersInfoComponent } from './components/users-info/users-info.component';
 import { UsersResolver } from './resolver/users.resolver';
-import { AuthGuard } from './guards/auth/auth.guard';
-import { LoginGuard } from './guards/auth/login.guard';
 
+// i am out of time to make lazy loading xD
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent, canActivate: [LoginGuard] },
