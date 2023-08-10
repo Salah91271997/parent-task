1090a61f23616ed3844d3ae3e8aedcac2d1aafd2
Salah Ebrahim <47260975+Salah91271997@users.noreply.github.com>
2023-08-10 14:29:12 +0300

    move api,token to environment

--- src/app/services/auth/auth-service.service.ts
+++ src/app/services/auth/auth-service.service.ts
@@ -2,15 +2,17 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+// import enviroment
 @Injectable({
   providedIn: 'root',
 })
 export class AuthServiceService {
-  private readonly authToken = 'QpwL5tke4Pnpja7X4';
-  private readonly apiUrl = 'https://reqres.in/api';
+  // private readonly authToken = 'QpwL5tke4Pnpja7X4';
+  // private readonly apiUrl = 'https://reqres.in/api';
   constructor(private http: HttpClient) {}
   getAuthToken(): string {
-    return this.authToken;
+    return environment.authToken;
   }
   // create a function hasToken() to check if the user has a token
   hasToken(): boolean {
@@ -19,7 +21,7 @@ export class AuthServiceService {
 
   login(username: string, password: string): Observable<any> {
     const body = { username, password };
-    return this.http.post<any>(`${this.apiUrl}/login`, body).pipe(
+    return this.http.post<any>(`${environment.apiUrl}/login`, body).pipe(
       map((response) => {
         const token = response.token;
         localStorage.setItem('authToken', token);
