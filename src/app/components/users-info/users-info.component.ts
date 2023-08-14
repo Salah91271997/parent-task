ea1083001dd08d81e3a1bfb6fc35bf562e46273c
Salah Ebrahim <47260975+Salah91271997@users.noreply.github.com>
2023-08-14 20:01:54 +0300

    fix update

--- src/app/components/users-info/users-info.component.ts
+++ src/app/components/users-info/users-info.component.ts
@@ -145,7 +145,7 @@ export class UsersInfoComponent implements OnInit {
         email: user.email,
         first_name: user.first_name,
         last_name: user.last_name,
-        job: '',
+        job: user?.job || '',
       });
       this.userForm.disable();
       this.userUpdate = false;
